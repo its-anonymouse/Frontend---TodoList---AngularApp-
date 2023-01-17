b4f262865e1c855a27b36e5edce3f2367b6d5b5c
Anonymouse <MOHITSHARMAFEB2004@GMAIL.COM>
2023-01-17 13:14:46 +0530

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TodolistMean
+# Todo-List
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.5.
 
@@ -25,3 +25,8 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## Built By - Mohit 
+LinkedIn - https://www.linkedin.com/in/mohit-sharma82/
+GitHub - https://github.com/its-anonymouse
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { NotifierService } from 'src/app/notifier.service';
+import { AuthData } from '../auth-data.model';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit, OnDestroy{
+
+  isLoading = false;
+
+  private authListenerSubs:Subscription;
+
+  constructor( public authService: AuthService,
+    private notifierService:NotifierService
+    ){
+
+  }
+
+  ngOnInit() {
+    this.authListenerSubs=this.authService
+    .getAuthStatusListener()
+    .subscribe(isAuthenticated=>{
+      this.isLoading=isAuthenticated;
+    })
+  }
+
+  onLogin(form:NgForm){
+    if(form.invalid){
+      if (form.value.password.length <=0) {
+        this.notifierService.showNotification("Please enter the correct password", "Okay!")
+        return;
+      }
+      else {
+        this.notifierService.showNotification("Please enter a valid email address ", "OK")
+        return;
+      }
+
+    }
+
+  this.isLoading = true;
+
+  const auth : AuthData = {
+    email: form.value.email,
+    password : form.value.password
+  }
+
+  this.authService.login(auth);
+
+
+}
+
+
+ngOnDestroy(){
+  this.authListenerSubs.unsubscribe()
+}
+
+}
